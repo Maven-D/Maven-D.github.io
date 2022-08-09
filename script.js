ae8a18c025e43522aa95a3fb0fe60fc4452b0ea0
Divit <divitajmera15@gmail.com>
2022-08-09 12:51:15 +0530

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const username = document.getElementsByClassName('username');
+const password = document.getElementsByClassName('password');
+
+// function submit() {
+//     location.href = "/about.html";
+// }
