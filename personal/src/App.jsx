3440a04ffa2cd4e8d2ee88f927573a1822963447
Divit <divitajmera15@gmail.com>
2023-06-15 11:48:40 +0530

    initial commit

---
+++ personal/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="place-content-center bg-slate-600 text-white">hello</div>
+  );
+}
+
+export default App;
---
+++ personal/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/App.jsx", "./src/components/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
